985f882a3eb810d4c2abe3cab7cc549639fd45a2
buchonsi <buchonsi@naver.com>
2022-05-23 09:41:50 +0900

    페이지

---
+++ src/test/java/com/cloncoding/book/springboot/web/dto/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.cloncoding.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void 메인페이지_로딩() {
+        //when
+        String body = restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
