fd10bb33f1630239006f8cceae768a8e35ef7594
buchonsi <buchonsi@naver.com>
2022-05-23 17:39:41 +0900

    oauth testing

---
+++ src/main/java/com/cloncoding/book/springboot/domain/user/User.java
@@ -0,0 +1,14 @@
+package com.cloncoding.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+
+}
